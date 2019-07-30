0f77364a0a1b1cd61c05740becc16344aec417d9
KonstantinKhudoyarov <kostya040390@gmail.com>
2019-07-30 15:40:32 +0300

    add delete-button to every item

--- src/App.js
+++ src/App.js
@@ -2,30 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    const list = [
-      {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-      },
-      {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-      }
-    ];
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: 'Angular',
+    url: 'https://angular.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 5,
+    points: 6,
+    objectID: 2
+  }
+];
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list
+    }
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID != id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({list: updatedList});
+  }
+
+  render() {
     return (
       <div className="App">
-        {list.map(item => {
+        {this.state.list.map(item => {
           return (
             <div key={item.objectID}>
               <span>
@@ -34,6 +58,13 @@ class App extends React.Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Удалить
+                </button>
+              </span>
             </div>
           );
         })}
